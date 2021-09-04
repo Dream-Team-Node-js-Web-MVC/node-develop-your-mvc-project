9541051017d9c50e3b13250f4a17d1f4dae32c87
김은영 <solaz0824@gmail.com>
2021-09-04 22:54:29 +0200

    hashed password for new workers

--- src/models/workerModel.js
+++ src/models/workerModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const WorkerSchema = new Schema(
   {
@@ -29,6 +30,22 @@ const WorkerSchema = new Schema(
   },
   { timestamp: true }
 );
+
+WorkerSchema.pre("save", function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    this.password = bcrypt.hashSync(this.password, 12);
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+WorkerSchema.methods.comparePassword = function comparePassword(pass) {
+  return bcrypt.compare(pass, this.password);
+};
 
 const Worker = mongoose.model("Worker", WorkerSchema);
 
